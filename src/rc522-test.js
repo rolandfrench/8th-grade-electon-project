b7231d227a3a665e0978cba2f5d23f746d1499f7
Scott French <mail@scottfrench.com>
2026-04-30 21:32:04 -0700

    another try

--- src/rc522-test.js
+++ src/rc522-test.js
@@ -1,30 +1,79 @@
 const SPI = require('pi-spi');
-
-// Open the hardware SPI device on Pi 5
 const spi = SPI.initialize('/dev/spidev0.0');
-spi.clockSpeed(1e6); // 1MHz
+spi.clockSpeed(1e6);
 
-console.log("--- Pi 5 RFID Raw Access ---");
+// MFRC522 Hex Commands
+const Commands = {
+    REQC: 0x26,      // Request Command
+    ANTICOLL: 0x93,  // Anticollision
+    TRANSCEIVE: 0x0C // Data transfer command
+};
 
-function readRegister(addr) {
-    return new Promise((resolve) => {
-        // MFRC522 address format: (addr << 1) & 0x7E | 0x80
-        const buffer = Buffer.from([(addr << 1) & 0x7E | 0x80, 0]);
-        spi.transfer(buffer, (err, data) => {
-            resolve(data[1]);
-        });
+// MFRC522 Registers
+const Regs = {
+    FIFOData: 0x09,
+    Command: 0x01,
+    BitFraming: 0x0D,
+    FIFOLevel: 0x0A
+};
+
+/**
+ * Basic SPI communication helpers
+ */
+function writeReg(addr, val) {
+    const buf = Buffer.from([(addr << 1) & 0x7E, val]);
+    spi.write(buf, () => {});
+}
+
+function readReg(addr) {
+    return new Promise(res => {
+        const buf = Buffer.from([(addr << 1) & 0x7E | 0x80, 0]);
+        spi.transfer(buf, (err, data) => res(data[1]));
     });
 }
 
-async function checkReader() {
-    // Read the Version register (0x37)
-    const version = await readRegister(0x37);
-    if (version === 0x00 || version === 0xFF) {
-        console.log("Reader NOT found. Check wiring!");
-    } else {
-        console.log(`Reader Found! Version: 0x${version.toString(16)}`);
-        console.log("The hardware is communicating correctly.");
-    }
+/**
+ * The Scan Logic
+ */
+async function requestTag() {
+    writeReg(Regs.Command, 0x00);         // Idle
+    writeReg(Regs.FIFOLevel, 0x80);       // Clear FIFO
+    writeReg(Regs.FIFOData, Commands.REQC);
+    writeReg(Regs.Command, Commands.TRANSCEIVE);
+    writeReg(Regs.BitFraming, 0x87);      // Start transmission
+
+    // Wait a tiny bit for hardware response
+    await new Promise(r => setTimeout(r, 50));
+
+    const level = await readReg(Regs.FIFOLevel);
+    return level > 0; // If FIFO has data, a card replied!
 }
 
-checkReader();
+async function getUID() {
+    writeReg(Regs.FIFOLevel, 0x80);
+    writeReg(Regs.FIFOData, Commands.ANTICOLL);
+    writeReg(Regs.FIFOData, 0x20); // NVB (Number of Valid Bits)
+    writeReg(Regs.Command, Commands.TRANSCEIVE);
+    writeReg(Regs.BitFraming, 0x00);
+
+    await new Promise(r => setTimeout(r, 50));
+
+    const level = await readReg(Regs.FIFOLevel);
+    if (level >= 5) {
+        const uid = [];
+        for (let i = 0; i < 4; i++) {
+            uid.push((await readReg(Regs.FIFOData)).toString(16).toUpperCase().padStart(2, '0'));
+        }
+        return uid.join(':');
+    }
+    return null;
+}
+
+// MAIN LOOP
+console.log("--- Scanning for Tags ---");
+setInterval(async () => {
+    if (await requestTag()) {
+        const id = await getUID();
+        if (id) console.log(`[${new Date().toLocaleTimeString()}] Tag Detected: ${id}`);
+    }
+}, 500);
