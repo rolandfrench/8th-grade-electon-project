3939cdf43dd201ad8b297929d1a00dd41d2c878c
Scott French <mail@scottfrench.com>
2026-05-02 19:33:04 -0700

    Emulator test

--- src/index.js
+++ src/index.js
@@ -146,7 +146,7 @@ app.whenReady().then(() => {
   createWindow();
   startPythonBridge();
 
-  setTimeout(10000, launchGame('launchGame'));
+  setTimeout(10000, () => launchGame('launchGame'));
 
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
